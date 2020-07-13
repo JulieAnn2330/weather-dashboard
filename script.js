812b0f61037b006e1989779563f220ad0ec5d33e
Julie Schaub <julie.schaub@hotmail.com>
2020-07-13 16:31:16 -0500

    updated files

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ var m = moment();
 
 $(function() {
     $("#tfq2b").click(function() {
-        if ($("#tfq2b").val() == "Enter Location"){
+        if ($("#tfq2b").val() == "Search By City"){
             $("#tfq2b").val(""); 
         }
     });
 });
+
+// OpenWeather API key: 19113027cee7d9c234b7c839da7576c2
 
 // Day, date, time
 $("#currentDay").text(moment().format('LLLL'));
